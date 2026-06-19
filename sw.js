8ece75d048fa1afbbcecc980717e95788a2970e8
BEN <215551633+drtybear@users.noreply.github.com>
2026-06-19 15:59:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 't-v2';
+const CACHE = 't-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
